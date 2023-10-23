6fd3ff907bb188430a7b0e88d02fc0c7856669f3
Alex Berkowitz <berky93@gmail.com>
2023-10-23 13:09:16 -0500

    Allow blurring/dimming of main bg while media is playing

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ const blankImage = "res/blank.jpg";
 albumArt[0].src = blankImage;
 albumBackgroundImage[0].src = blankImage;
 
+// Stored values
+shouldDimBg = true;
+shouldBlurBg = true;
+
 
 // Get current album info
 function livelyCurrentTrack(data) {
@@ -16,24 +20,39 @@ function livelyCurrentTrack(data) {
   if (obj != null && obj.Thumbnail != null) {
     albumArt[0].src = "data:image/png;base64, " + obj.Thumbnail;
     albumBackgroundImage[0].src = "data:image/png;base64, " + obj.Thumbnail;
-    setAlbumVisibility(true);
+    handleIsPlaying(true);
   } else {
-    setAlbumVisibility(false);
+    handleIsPlaying(false);
   }
 }
 
 // Control visibility
-function setAlbumVisibility(shouldShow) {
-  if( shouldShow ){
+function handleIsPlaying(isPlaying) {
+  if( isPlaying ){
     albumContainer.css("opacity", 1);
+
+    if( shouldDimBg ){
+      backgroundImage.removeClass('disableDimEffect');
+    }
+      
+    if( shouldBlurBg ){
+      backgroundImage.removeClass('disableBlurEffect');
+    }
+
   } else {
     albumContainer.css("opacity", 0);
+    backgroundImage.addClass('disableDimEffect');
+    backgroundImage.addClass('disableBlurEffect');
   }
 }
 
 // Handle properties
 function livelyPropertyListener(name, val) {
   switch(name) {
+    case 'showAlbumArt':
+      albumArt.css('display', val ? 'block' : 'none');
+      break;
+
     case 'albumSize':
       albumArt.css('height', `${val}%`);
       break;
@@ -46,18 +65,34 @@ function livelyPropertyListener(name, val) {
       backgroundImage[0].src = val;
       break;
     
-    case 'useAlbumArtAsBg':
-      const shouldShow = val ? 'block' : 'none';
-      const containerBgColor = val ? 'black' : 'transparent';
-      albumBackgroundImage.css('display', shouldShow);
-      albumContainer.css('background-color', containerBgColor);
+    case "dimBackgroundWhilePlaying":
+      if( val ){
+        backgroundImage.addClass('dimBg');
+      } else {
+        backgroundImage.removeClass('dimBg');
+      }
+      break;
+
+    case "blurBackgroundWhilePlaying":
+      if( val ){
+        backgroundImage.addClass('blurBg');
+      } else {
+        backgroundImage.removeClass('blurBg');
+      }
+      break;
+    
+    case 'showAlbumArtBg':
+      albumBackgroundImage.css('display', val ? 'block' : 'none');
+      albumContainer.css('background-color', val ? 'black' : 'transparent');
       break;
 
     case "bgBrightness":
+      backgroundImage.css('opacity', val);
       albumBackgroundImage.css('opacity', val);
       break;
 
     case "bgBlur":
+      backgroundImage.css('filter', `blur(${val}px)`);
       albumBackgroundImage.css('filter', `blur(${val}px)`);
       break;
   }
