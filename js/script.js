7179179cbff05a99f6eea8d3b5ec2009506dd7b9
Alex Berkowitz <berky93@gmail.com>
2023-10-23 10:56:43 -0500

    add customization options

--- js/script.js
+++ js/script.js
@@ -1,21 +1,64 @@
-const albumArt = document.getElementById("albumart");
-const backgroundImage = document.getElementById("backgroundImage");
+const albumContainer = $("#albumContainer");
+const albumArt = $("#albumArt");
+const albumBackgroundImage = $("#albumBackgroundImage");
+const backgroundImage = $("#backgroundImage");
 
-let blankImage = "res/blank.jpg";
-albumArt.src = blankImage;
-backgroundImage.src = blankImage;
+const blankImage = "res/blank.jpg";
 
+// Setup values
+albumArt[0].src = blankImage;
+albumBackgroundImage[0].src = blankImage;
+
+
+// Get current album info
 function livelyCurrentTrack(data) {
   let obj = JSON.parse(data);
   if (obj != null && obj.Thumbnail != null) {
-    $("#albumContainer").css("opacity", 1);
-    albumArt.src = "data:image/png;base64, " + obj.Thumbnail;
-    backgroundImage.src = "data:image/png;base64, " + obj.Thumbnail;
+    albumArt[0].src = "data:image/png;base64, " + obj.Thumbnail;
+    albumBackgroundImage[0].src = "data:image/png;base64, " + obj.Thumbnail;
+    setAlbumVisibility(true);
   } else {
-    $("#albumContainer").css("opacity", 0);
+    setAlbumVisibility(false);
   }
 }
 
-function getFileExtension(filename) {
-  return filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
+// Control visibility
+function setAlbumVisibility(shouldShow) {
+  if( shouldShow ){
+    albumContainer.css("opacity", 1);
+  } else {
+    albumContainer.css("opacity", 0);
+  }
+}
+
+// Handle properties
+function livelyPropertyListener(name, val) {
+  switch(name) {
+    case 'albumSize':
+      albumArt.css('height', `${val}%`);
+      break;
+
+    case 'albumCornerRadius':
+      albumArt.css('border-radius', `${val}%`);
+      break;
+    
+    case "bgImage":
+      backgroundImage[0].src = val;
+      break;
+    
+    case 'useAlbumArtAsBg':
+      const shouldShow = val ? 'block' : 'none';
+      const containerBgColor = val ? 'black' : 'transparent';
+      albumBackgroundImage.css('display', shouldShow);
+      albumContainer.css('background-color', containerBgColor);
+      break;
+
+    case "bgBrightness":
+      albumBackgroundImage.css('opacity', val);
+      break;
+
+    case "bgBlur":
+      albumBackgroundImage.css('filter', `blur(${val}px)`);
+      break;
+  }
 }
